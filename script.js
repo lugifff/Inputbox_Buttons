0d2d61d11c7b056c0e54335b77daed7d004c5db5
lugifff <luigifan826@gmail.com>
2024-07-13 12:38:40 -0300

    Boot ons

---
+++ script.js
@@ -0,0 +1,10 @@
+let Action = document.querySelector(".Action");
+let Lawsuit = document.querySelector(".Lawsuit");
+
+
+function wheat() {
+    console.log(Lawsuit.value);
+}
+
+Action.addEventListener("click", wheat);
+
